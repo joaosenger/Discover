475d494a0c1277170e88195dfee9eeee16058d7b
João Senger <74719360+joaosenger@users.noreply.github.com>
2022-03-25 18:03:30 -0300

    Create array.js

---
+++ guiaEstelarJs/tipos-de-dados/array.js
@@ -0,0 +1,12 @@
+/*
+São vetores.
+
+    Array
+        - uma lista
+        - agrupamento de dados
+
+        ['João', 27]
+*/
+
+let joao = ['João', 27, 'Programador']
+console.log("Nome: " + joao[0] + ", idade: " + joao[1] + ", profissão: " + joao[2]);
