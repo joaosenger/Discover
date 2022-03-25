0eded3abfb70b6b1d44ae8e266db68f58d2c684b
João Senger <74719360+joaosenger@users.noreply.github.com>
2022-03-25 18:04:31 -0300

    Create comentarios.js

---
+++ guiaEstelarJs/primeiros-passos/comentarios.js
@@ -0,0 +1,9 @@
+// comentário em uma única linha
+
+/* Comentário
+em várias linhas,
+também chamado de
+comentário em bloco
+*/
+
+// console.log("Usado também para inutilizar uma parte do código, debuggando")
